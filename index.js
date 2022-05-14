7a5e9ecbcafdc6cba15bbf5eede2539d778069aa
kartikgowda <kartik.gowda55@gmail.com>
2022-05-14 14:07:56 +1000

    Selecting all the pads on the drum set for assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert('Hello there how you doing?');
+// document.querySelector('.drum').addEventListener('click', function () {
+//   alert('You clicked a button!');
+// });
+
+// let numberOfButtons = document.querySelectorAll('.drum');
+
+// // //? Selecting all the buttons
+// for (let i = 0; i < numberOfButtons.length; i++) {
+//   numberOfButtons[i].addEventListener('click', function () {
+//     alert('You have been clicked');
+//   });
+// }
+
+//? Refactoring Selections of all Buttons
+document.querySelectorAll('button').forEach((button) => {
+  button.addEventListener('click', function () {
+    alert('I got clicked baby');
+  });
+});
